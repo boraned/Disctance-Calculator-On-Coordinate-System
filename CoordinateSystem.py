619365e943c05e0b90bec01f1b35ee7c42707163
Boran <49809633+boraned@users.noreply.github.com>
2022-11-13 19:16:50 -0800

    Create CoordinateSystem.py

---
+++ CoordinateSystem.py
@@ -0,0 +1,75 @@
+def PrimeCheck(num: int):
+    for n in range(2, num-1):
+        if num % n == 0:
+            return False
+    return True
+
+def NextPrime(num: int):
+    temp = num+1
+    while not PrimeCheck(temp):
+        temp += 1
+    return temp
+
+class twoNumbers:
+    def __init__(self, first: int, second: int) -> None:
+        self.first = first
+        self.second = second
+    def __str__(self) -> str:
+        return f"{self.first}, {self.second}"
+
+def perfectSquare(num):
+    outOfRoot = 1
+    prime = 1
+
+    while prime**2 < num:
+        prime = NextPrime(prime)
+        if num % prime**2 == 0:
+            outOfRoot *= prime
+            num /= prime**2
+            prime = 1
+    return twoNumbers(outOfRoot, int(num))
+
+class SquareRoot:
+    def __init__(self, value) -> None:
+        self.outOfRoot = perfectSquare(value).first
+        self.inRoot = perfectSquare(value).second
+
+    def __repr__(self) -> str:
+        representInRoot = self.inRoot
+        if representInRoot == 1:
+            return f"{self.outOfRoot}"
+        elif self.outOfRoot == 1:
+            return f"√{self.inRoot}"
+        else:
+            return f"{self.outOfRoot}√{self.inRoot}"
+  
+def Difference(num: int, num2: int):
+    diff = num - num2
+    if diff < 0:
+        diff = -diff
+    return diff
+
+class Coordinates2D:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+    
+    def goTo(self, x: int, y:int):
+        xWay = Difference(x, self.x)
+        yWay = Difference(y, self.y)
+        wayTotal = SquareRoot(xWay**2 + yWay**2)
+        return f"You have to move {wayTotal} units."
+
+
+class Coordinates3D:
+    def __init__(self, x, y, z) -> None:
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def goTo(self, x: int, y:int, z: int):
+        xWay = Difference(x, self.x)
+        yWay = Difference(y, self.y)
+        zWay = Difference(z, self.z)
+        wayTotal = SquareRoot(xWay**2 + yWay**2 + zWay**2)
+        return f"You have to move {wayTotal} units."
